33e440ead6c707c62176bbd681402f95dc9f4712
Jonathan Calle <jonathanc641@nycstudents.net>
2023-10-24 19:32:58 -0400

    first commit

---
+++ Project3/script.js
@@ -0,0 +1,24 @@
+const startButton = document.getElementById("start");
+let userTable, botTable, score, roundLog; 
+
+
+function initialize(){  
+    startButton.addEventListener("click"); 
+    userTable = document.getElementById("player");
+    botTable = document.getElementById("computer");  
+    roundLog = document.getElementById("log"); 
+
+    score = 0; 
+
+}
+
+
+
+function addItem(){}
+
+function removeItem(){}
+
+
+function computerChoice(){}
+
+function game(){}
