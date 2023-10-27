ab8981461cabfca46ef6498a1e2f43a7ccaddc1e
Jonathan Calle <jonathanc641@nycstudents.net>
2023-10-26 20:53:01 -0400

    New code!

--- Project3/script.js
+++ Project3/script.js
@@ -1,24 +1,49 @@
-const startButton = document.getElementById("start");
-let userTable, botTable, score, roundLog; 
+const weapons = ['rock', 'paper', 'scissors']; 
+let userTable, botTable, score, roundLog, itemCount, count, weaponChoice;  
 
 
 function initialize(){  
-    startButton.addEventListener("click"); 
     userTable = document.getElementById("player");
     botTable = document.getElementById("computer");  
     roundLog = document.getElementById("log"); 
 
     score = 0; 
+    count = 0;
 
 }
 
 
 
-function addItem(){}
+function addItem(){
+    count+=1; 
+}
 
-function removeItem(){}
-
+function removeItem(){
+    count-=1; 
+}
 
 function computerChoice(){}
+function userChoice(){}
 
-function game(){}
+function fight(user,bot){
+    if(user === bot){
+        return false; 
+    }
+
+}
+
+function game(weapon){
+    itemCount = document.getElementById(weapon); 
+
+    let userWeapon = weapon === 'rock' ? 'rock' : weapon === 'paper' ? 'scissors' : 'paper'; 
+    console.log(userWeapon); 
+    let botChoice = computerChoice(); 
+    
+    
+    
+
+}
+
+function display(){
+    itemCount.innerHTML = count; 
+}
