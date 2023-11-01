97d9863874ef3be9dfb69436deafc7551d2c3a4b
Jonathan Calle <jonacalc@amazon.com>
2023-11-01 16:37:13 -0400

    Implemented the tool uses feature

--- Project3/script.js
+++ Project3/script.js
@@ -4,16 +4,17 @@ const choiceDealer = new Map();
 const imgTools = new Map(); 
 const usesPerWeapon = new Map(); 
 const botUsesPerWeapon = new Map(); 
-let userTable, botTable, score, roundLog, playerItemCount, botItemCount, playerCount, botCount, weaponChoice, botWeaponDisplay, userWeaponDisplay, useCount, weaponUsed, messageBoard;  
+let userTable, botTable, score, roundLog, starterItemCount, playerItemCount, botItemCount, playerCount, botCount, weaponChoice, botWeaponDisplay, userWeaponDisplay, useCount, weaponUsed, messageBoard;  
 
 
 function initialize(){  
     userTable = document.getElementById("player");
     botTable = document.getElementById("computer");  
-    roundLog = document.querySelector("roundTracker tr");
+    roundLog = document.querySelector(".roundTracker tr");
     botWeaponDisplay = document.getElementById("botTool"); 
     userWeaponDisplay = document.getElementById("userTool"); 
     messageBoard = document.getElementById("status"); 
+    starterItemCount = document.querySelectorAll("#count");
 
     for(let i = 0; i < weapons.length; i++){
         imgTools.set(weapons[i], images[i]); 
@@ -22,10 +23,9 @@ function initialize(){
         botUsesPerWeapon.set(weapons[i],3); 
     }
 
-
     console.log(imgTools.keys());
     console.log(imgTools.values());
-    score = 0; 
+
     playerCount = 0;
     botCount = 0; 
     useCount = 0; 
@@ -36,30 +36,25 @@ function updateLog(newLog){
     newCell.innerHTML = newLog; 
 }
 
-
-function manageUses(player,operation){
+function updateUses(list, tool){
     let currentVal = 0; 
     let newVal =0; 
-
-    if(player === 'bot'){
-        currentVal = botUsesPerWeapon.get(p2);
-        if(currentVal !== 0){
-            newVal = currentVal-1; 
-            botUsesPerWeapon.set(p2,newVal); 
-            botCount = botUsesPerWeapon.get(p2); 
-        }else{
-
-        }
-    }else{
-        currentVal = usesPerWeapon.get(p1);
-        if(currentVal !== 0){
-            newVal = currentVal-1; 
-            usesPerWeapon.set(p1,newVal); 
-            playerCount = usesPerWeapon.get(p1);
-             
-        }
+    currentVal = list.get(tool);
+    if(currentVal !== 0){
+        newVal = currentVal-1; 
+        list.set(tool,newVal); 
     }
+}
 
+function manageUses(player,tool,tool2){
+    if(player === 'player'){
+        updateUses(usesPerWeapon, tool);
+    }else if(player === 'bot'){
+        updateUses(botUsesPerWeapon, tool);
+    }else{
+        updateUses(usesPerWeapon, tool); 
+        updateUses(botUsesPerWeapon, tool2); 
+    }
 
 }
 
@@ -72,18 +67,21 @@ function computerChoice(){
 function fight(p1,p2){
     let playerChoice = choiceDealer.get(p1); 
     let botChoice = choiceDealer.get(p2);
+
+    console.log("User Value:", playerChoice); 
+    console.log("Bot Value:", botChoice);
     let result = (playerChoice + 1 ) % 3; 
 
-    if(botChoice === result){
-        updateLog(`Steve won using ${p2}`);
-    }else if(playerChoice === result){
-        updateLog(`Player won using ${p1}`);
-    }else{
+    if(botChoice === playerChoice){
         updateLog("Tie");
+        manageUses('both',p1,p2);
+    }else if(botChoice === result){
+        updateLog(`Steve won using ${p2}`);
+        manageUses('bot',p2); 
+    }else{
+        updateLog(`Player won using ${p1}`);
+        manageUses('player',p1); 
     }
-
-    manageUses();
-
 }
 
 
@@ -91,20 +89,22 @@ function game(weapon){
     setTimeout(() => {
         let selectedWeapon = weapon.slice(0,weapon.length-4); 
         let userWeapon = selectedWeapon === 'rock' ? 'rock' : selectedWeapon === 'paper' ? 'paper' : 'scissors'; 
-        console.log(userWeapon); 
+        console.log("User weapon:", userWeapon); 
         let botWeapon = computerChoice(); 
+        console.log("Bot Weapon:", botWeapon); 
         userWeaponDisplay.src = imgTools.get(userWeapon); 
         botWeaponDisplay.src = imgTools.get(botWeapon); 
         fight(userWeapon,botWeapon);
         // display();
     }, 450);
+    userWeaponDisplay.src = ""; 
+    botWeaponDisplay.src = ""; 
      
 }
 
 
 function reset(){
-    playerCount =0; 
-    botCount = 0; 
+    starterItemCount.innerHTML = 5;
 }
 
 function display(){
