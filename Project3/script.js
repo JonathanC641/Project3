818831270fce602f0dd58b561f7211ec9c481e66
Jonathan Calle <jonathanc641@nycstudents.net>
2023-11-01 23:43:38 -0400

    New code!

--- Project3/script.js
+++ Project3/script.js
@@ -4,17 +4,24 @@ const choiceDealer = new Map();
 const imgTools = new Map(); 
 const usesPerWeapon = new Map(); 
 const botUsesPerWeapon = new Map(); 
-let userTable, botTable, score, roundLog, starterItemCount, playerItemCount, botItemCount, playerCount, botCount, weaponChoice, botWeaponDisplay, userWeaponDisplay, useCount, weaponUsed, messageBoard;  
+let userTable, botTable, score, roundLog, playerItemCount, botItemCount, playerCount, botCount, weaponChoice, botWeaponDisplay, userWeaponDisplay, weaponUsed, messageBoard, resetButton;  
 
 
 function initialize(){  
     userTable = document.getElementById("player");
     botTable = document.getElementById("computer");  
-    roundLog = document.querySelector(".roundTracker tr");
+    roundLog = document.querySelector(".roundLog tr");
     botWeaponDisplay = document.getElementById("botTool"); 
     userWeaponDisplay = document.getElementById("userTool"); 
     messageBoard = document.getElementById("status"); 
-    starterItemCount = document.querySelectorAll("#count");
+    resetButton = document.getElementById("resetBtn");
+
+    resetButton.addEventListener("mouseover", function(){
+        this.style.backgroundColor = "green";
+    });
+    resetButton.addEventListener("mouseout", function(){
+        this.style.backgroundColor = "red"; 
+    });
 
     for(let i = 0; i < weapons.length; i++){
         imgTools.set(weapons[i], images[i]); 
@@ -26,14 +33,43 @@ function initialize(){
     console.log(imgTools.keys());
     console.log(imgTools.values());
 
-    playerCount = 0;
-    botCount = 0; 
-    useCount = 0; 
+    playerCount = 5; 
+    botCount = 5;  
 }
 
 function updateLog(newLog){
     let newCell = roundLog.insertCell();
     newCell.innerHTML = newLog; 
+}
+
+function updateMessageBoard(newMsg){
+    setTimeout(()=>{
+        messageBoard.innerHTML = newMsg; 
+    },100); 
+}
+
+function updateItemCount(player, item, func){
+    if(player === 'bot'){
+        botItemCount = document.getElementById(`${item}Bot`);
+        if(func === 'add'){
+            botCount+=1; 
+        }else if(botCount !== 0 && func === 'sub'){
+            botCount-=1; 
+        }else{
+            botCount = 0; 
+        }
+        botItemCount.innerHTML = botCount; 
+    }else{
+        playerItemCount = document.getElementById(`${item}User`);
+        if(func === 'add'){
+            playerCount+=1; 
+        }else if(playerCount !== 0 && func === 'sub'){
+            playerCount-=1; 
+        }else{
+            playerCount = 0; 
+        }
+        playerItemCount.innerHTML = playerCount; 
+    }
 }
 
 function updateUses(list, tool){
@@ -43,6 +79,13 @@ function updateUses(list, tool){
     if(currentVal !== 0){
         newVal = currentVal-1; 
         list.set(tool,newVal); 
+    }else{
+        if(list === botUsesPerWeapon){
+            updateItemCount('bot', tool, 'sub');
+        }else{
+            updateItemCount('player', tool, 'sub');
+        }
+        list.set(tool,3); 
     }
 }
 
@@ -59,10 +102,8 @@ function manageUses(player,tool,tool2){
 }
 
 function computerChoice(){
-    let randNum = parseInt(Math.random() * 3); 
-    return weapons[randNum]; 
+    return weapons[parseInt(Math.random() *3)]; 
 }
-
 
 function fight(p1,p2){
     let playerChoice = choiceDealer.get(p1); 
@@ -73,14 +114,21 @@ function fight(p1,p2){
     let result = (playerChoice + 1 ) % 3; 
 
     if(botChoice === playerChoice){
-        updateLog("Tie");
+        updateLog(`Steve: ${p2}\nPlayer: ${p1}\nWinner: Tie`);
         manageUses('both',p1,p2);
+        updateMessageBoard("No winners :("); 
     }else if(botChoice === result){
-        updateLog(`Steve won using ${p2}`);
+        updateLog(`Steve: ${p2}\nPlayer: ${p1}\nWinner: Steve`);
+        updateMessageBoard("Steve won!");
         manageUses('bot',p2); 
+        updateItemCount('bot', p1, 'add');
+        updateItemCount('player', p1, 'sub');  
     }else{
-        updateLog(`Player won using ${p1}`);
-        manageUses('player',p1); 
+        updateLog(`Steve: ${p2}\nPlayer:${p1}\nWinner: Player`);
+        updateMessageBoard("You won!");
+        manageUses('player',p1);
+        updateItemCount('player', p2, 'add'); 
+        updateItemCount('bot', p2, 'sub'); 
     }
 }
 
@@ -89,25 +137,25 @@ function game(weapon){
     setTimeout(() => {
         let selectedWeapon = weapon.slice(0,weapon.length-4); 
         let userWeapon = selectedWeapon === 'rock' ? 'rock' : selectedWeapon === 'paper' ? 'paper' : 'scissors'; 
-        console.log("User weapon:", userWeapon); 
         let botWeapon = computerChoice(); 
-        console.log("Bot Weapon:", botWeapon); 
         userWeaponDisplay.src = imgTools.get(userWeapon); 
         botWeaponDisplay.src = imgTools.get(botWeapon); 
         fight(userWeapon,botWeapon);
-        // display();
-    }, 450);
+    }, 150);
     userWeaponDisplay.src = ""; 
     botWeaponDisplay.src = ""; 
-     
 }
 
 
 function reset(){
-    starterItemCount.innerHTML = 5;
+    for(let i=1;  i<numOfLogs; i++){
+        roundLog.deleteCell(i); 
+    }
+    userWeaponDisplay.src = ""; 
+    botWeaponDisplay.src = ""; 
+    playerItemCount.innerHTML = 5; 
+    botItemCount.innerHTML = 5; 
+    
 }
 
-function display(){
-    playerItemCount.innerHTML = playerCount; 
-    botItemCount.innerHTML = botCount;
-}
+
